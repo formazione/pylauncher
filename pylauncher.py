a911bce22637401946892e3d83f3e52805521e25
Gio Ga <gatto.gio@gmail.com>
2023-08-09 19:05:14 +0200

    new commit

--- pylauncher.py
+++ pylauncher.py
@@ -1,36 +1,44 @@
+# pylauncher
+# crate a button for every python script; click to open file
+
 import os
 import tkinter as tk
 import tkinter.ttk as ttk
 from glob import glob
 
 root = tk.Tk()
-root.title("Launcher")
 root.geometry("400x400")
 
+numfile = 0
 class Button:
 
   def __init__(self, file, row, col):
     self.file = file
+    self.row = row
+    self.col = col
+    self.button()
+
+  def button(self):
     self.button = ttk.Button(
         root,
-        text=file,
-        command=self.open_text)
-    self.button.grid(row=row, column=col, sticky=tk.NW, pady=0)
+        text=self.file,
+        command=self.open_pythonfile)
+    self.position()
 
-  def open_text(self):
-    # with open(self.file) as file:
-    #   testo = file.read()
-    # area.insert(tk.END, testo)
-    os.startfile(self.file)
+  def position(self):
+    self.button.grid(row=self.row,
+        column=self.col,
+        sticky=tk.NW, pady=0)
 
-# area = tk.Text()
-# area.grid(row=0, column=1)
+  def open_pythonfile(self):
+    os.system(f"start {self.file}")
 
 
 files = glob("*.py")
-print(files)
 for n, fname in enumerate(files):
   Button(fname, n, 0)
+  numfile += 1
 
+root.title(f"PyLauncher - num. Files: {numfile}")
 
 root.mainloop()
